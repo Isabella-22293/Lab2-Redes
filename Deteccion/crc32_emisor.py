b99b6d67f381f92a14b47024b7ba3b171e098024
Anaru03 <dele22428@uvg.edu.gt>
2025-07-31 09:18:16 -0600

    Agregar CRC32 y simulación de ruido

--- Deteccion/crc32_emisor.py
+++ Deteccion/crc32_emisor.py
@@ -1,18 +1,42 @@
-def crc32_emisor(data_binaria): 
-    polinomio = 0x104C11DB7 
-    data = data_binaria + '0' * 32 
-    data = list(map(int, data)) 
+import random
 
-    for i in range(len(data_binaria)): 
-        if data[i] == 1: 
-            for j in range(33): 
-                data[i + j] ^= (polinomio >> (32 - j)) & 1 
+# ===== Capa Aplicación =====
+def solicitar_mensaje():
+    mensaje = input("Ingrese el mensaje a enviar: ")
+    return mensaje
 
-    crc = ''.join(map(str, data[-32:])) 
-    return data_binaria + crc 
+# ===== Capa Presentación =====
+def codificar_ascii_binario(mensaje):
+    return ''.join(format(ord(c), '08b') for c in mensaje)
 
-# Ejecución de prueba
+# ===== Capa Enlace (CRC32) =====
+def crc32_emisor(data_binaria):
+    polinomio = 0x104C11DB7
+    data = data_binaria + '0' * 32
+    data = list(map(int, data))
+
+    for i in range(len(data_binaria)):
+        if data[i] == 1:
+            for j in range(33):
+                data[i + j] ^= (polinomio >> (32 - j)) & 1
+
+    crc = ''.join(map(str, data[-32:]))
+    return data_binaria + crc
+
+# ===== Capa Ruido =====
+def aplicar_ruido(trama, prob_error=0.01):
+    trama_lista = list(trama)
+    for i in range(len(trama_lista)):
+        if random.random() < prob_error:
+            trama_lista[i] = '0' if trama_lista[i] == '1' else '1'
+    return ''.join(trama_lista)
+
+# ===== Ejecución Emisor =====
 if __name__ == "__main__":
-    mensaje = "11010011101100"
-    resultado = crc32_emisor(mensaje)
-    print("Trama con CRC:", resultado)
+    mensaje = solicitar_mensaje()
+    binario = codificar_ascii_binario(mensaje)
+    trama_crc = crc32_emisor(binario)
+    trama_con_ruido = aplicar_ruido(trama_crc, prob_error=0.02)
+
+    print("\n[EMISOR] Trama generada:", trama_crc)
+    print("[EMISOR] Trama con ruido:", trama_con_ruido)
