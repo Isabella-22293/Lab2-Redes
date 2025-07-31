4075bd369610ccc9a5b55d351cd73fd86d197bc2
Anaru03 <dele22428@uvg.edu.gt>
2025-07-31 09:40:49 -0600

    Decodificar binario a texto si no hay error

--- Deteccion/receptor.cpp
+++ Deteccion/receptor.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <bitset>
 using namespace std;
 
+// ===== Capa Enlace =====
 unsigned int crc32_receptor(const string& input) {
     unsigned int poly = 0x04C11DB7;
     unsigned int crc = 0;
@@ -16,13 +18,29 @@ unsigned int crc32_receptor(const string& input) {
     }
     return crc;
 }
+
+// ===== Capa Presentación =====
+string decodificar_ascii_binario(const string& binario) {
+    string mensaje;
+    for (size_t i = 0; i < binario.size(); i += 8) {
+        bitset<8> bits(binario.substr(i, 8));
+        mensaje += char(bits.to_ulong());
+    }
+    return mensaje;
+}
+
+// ===== Ejecución Receptor =====
 int main() {
     string trama;
-    cout << "Ingrese la trama con CRC: ";
-    cin >> trama;   
-    if (crc32_receptor(trama) == 0)
-        cout << "Trama valida: sin errores." << endl;
-    else
-        cout << "Error detectado: trama invalida." << endl;
+    cout << "Ingrese la trama recibida: ";
+    cin >> trama;
+
+    if (crc32_receptor(trama) == 0) {
+        string mensaje_original = decodificar_ascii_binario(trama.substr(0, trama.size() - 32));
+        cout << "[RECEPTOR] Trama válida. Mensaje: " << mensaje_original << endl;
+    } else {
+        cout << "[RECEPTOR] Error detectado: trama inválida." << endl;
+    }
+
     return 0;
 }
